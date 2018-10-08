87f3a8572c58328b5abea45d78fde45cdeb192c8
Unknown <Luther.ninja@gmail.com>
2018-10-08 14:20:31 -0600

    Got something to instantiate indefinitely

---
+++ WordGameBUGZ/Assets/Scripts/InstanceObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+//Made By Jonathan Luther
+public class InstanceObject :ScriptableObject
+{
+
+	public void Call(GameObject instance)
+	{
+		Instantiate(instance);
+	}
+}
---
+++ WordGameBUGZ/Assets/Scripts/PerpetualCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PerpetualCounter : MonoBehaviour
+{
+
+	public UnityEvent OnCount;
+	public float Seconds;
+
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			OnCount.Invoke();
+			yield return new WaitForSeconds(Seconds);
+		}
+	}
+}
